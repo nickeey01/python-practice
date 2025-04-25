f0f3312a02edaff044e122e86e333d23a3fb6872
Nicole <nicole.omuoyo@strathmore.edu>
2025-04-25 14:13:20 +0300

    commit 1

---
+++ sum in a list.py
@@ -0,0 +1,7 @@
+#sum of elements in a list
+#First define the elements in the list and assign it a variable
+my_list = [1,2,3,4,5]
+#find the sum of elements in the list
+sum_of_list = sum(my_list)
+#print the sum
+print(sum_of_list)
